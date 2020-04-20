c600a7ccb4faf4ec2a889cad10a0293b5bbde744
Gutyerrez <srgutyerrez@gmail.com>
2020-04-20 12:16:01 -0300

    Create StartManager.java

---
+++ src/main/java/com/redefocus/factionscaribe/manager/StartManager.java
@@ -0,0 +1,23 @@
+package com.redefocus.factionscaribe.manager;
+
+public class StartManager {
+    public StartManager() {
+
+    }
+}
+
+class ListenerManager {
+
+}
+
+class CommandManager {
+
+}
+
+class TableManager {
+
+}
+
+class DataManager {
+
+}
