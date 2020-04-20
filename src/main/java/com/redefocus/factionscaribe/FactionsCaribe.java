2c6f8381d3c88bf987d083fa5f14bde43cf58c45
Gutyerrez <srgutyerrez@gmail.com>
2020-04-20 12:15:39 -0300

    Update FactionsCaribe.java

--- src/main/java/com/redefocus/factionscaribe/FactionsCaribe.java
+++ src/main/java/com/redefocus/factionscaribe/FactionsCaribe.java
@@ -1,11 +1,20 @@
 package com.redefocus.factionscaribe;
 
 import com.redefocus.api.spigot.FocusPlugin;
+import com.redefocus.factionscaribe.manager.StartManager;
+import lombok.Getter;
 
 public class FactionsCaribe extends FocusPlugin {
+    @Getter
+    private static FactionsCaribe instance;
+
+    public FactionsCaribe() {
+        FactionsCaribe.instance = this;
+    }
+
     @Override
     public void onEnablePlugin() {
-
+        new StartManager();
     }
 
     @Override
