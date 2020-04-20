312fbce5b44de1715d6b22178539f50135b4dc1f
Gutyerrez <srgutyerrez@gmail.com>
2020-04-20 08:03:00 -0300

    Create pom.xml

---
+++ src/main/java/com/redefocus/factionscaribe/FactionsCaribe.java
@@ -0,0 +1,15 @@
+package com.redefocus.factionscaribe;
+
+import com.redefocus.api.spigot.FocusPlugin;
+
+public class FactionsCaribe extends FocusPlugin {
+    @Override
+    public void onEnablePlugin() {
+
+    }
+
+    @Override
+    public void onDisablePlugin() {
+
+    }
+}
