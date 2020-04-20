451de874b0bb5d9b0a84a33c5f7de4aa33c4f2b9
Gutyerrez <srgutyerrez@gmail.com>
2020-04-20 12:14:57 -0300

    Create CaribeUserFactory.java

---
+++ src/main/java/com/redefocus/factionscaribe/user/factory/CaribeUserFactory.java
@@ -0,0 +1,33 @@
+package com.redefocus.factionscaribe.user.factory;
+
+import com.google.common.collect.Lists;
+import com.redefocus.common.shared.permissions.user.factory.AbstractUserFactory;
+import com.redefocus.factionscaribe.user.data.CaribeUser;
+
+import java.util.List;
+import java.util.UUID;
+
+public class CaribeUserFactory<U extends CaribeUser> extends AbstractUserFactory<U> {
+    private List<U> users = Lists.newArrayList();
+
+    @Override
+    public U getUser(Integer id) {
+        return this.users.stream()
+                .filter(u -> u.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public U getUser(String name) {
+        return this.users.stream()
+                .filter(u -> u.getName().equalsIgnoreCase(name))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public U getUser(UUID uuid) {
+        return null;
+    }
+}
