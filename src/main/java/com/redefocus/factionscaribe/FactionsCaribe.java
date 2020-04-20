c32f97038c7eb172f9f6157eecf06428701d72dc
Gutyerrez <srgutyerrez@gmail.com>
2020-04-20 12:58:02 -0300

    Update FactionsCaribe.java

--- src/main/java/com/redefocus/factionscaribe/FactionsCaribe.java
+++ src/main/java/com/redefocus/factionscaribe/FactionsCaribe.java
@@ -2,6 +2,8 @@ package com.redefocus.factionscaribe;
 
 import com.redefocus.api.spigot.FocusPlugin;
 import com.redefocus.factionscaribe.manager.StartManager;
+import com.redefocus.factionscaribe.user.data.CaribeUser;
+import com.redefocus.factionscaribe.user.factory.CaribeUserFactory;
 import lombok.Getter;
 
 public class FactionsCaribe extends FocusPlugin {
@@ -12,9 +14,17 @@ public class FactionsCaribe extends FocusPlugin {
         FactionsCaribe.instance = this;
     }
 
+    @Getter
+    private CaribeUserFactory<? extends CaribeUser> caribeUserFactory;
+
+    @Getter
+    private static final Long startTime = System.currentTimeMillis();
+
     @Override
     public void onEnablePlugin() {
         new StartManager();
+
+        this.caribeUserFactory = new CaribeUserFactory<>();
     }
 
     @Override
