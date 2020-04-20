8ace38f77c035049ba65a7122f13d0fcdb3b7155
Gutyerrez <srgutyerrez@gmail.com>
2020-04-20 12:56:49 -0300

    Create VanishCommand.java

---
+++ src/main/java/com/redefocus/factionscaribe/commands/staff/VanishCommand.java
@@ -0,0 +1,27 @@
+package com.redefocus.factionscaribe.commands.staff;
+
+import com.redefocus.api.spigot.commands.CustomCommand;
+import com.redefocus.api.spigot.commands.enums.CommandRestriction;
+import com.redefocus.common.shared.permissions.group.GroupNames;
+import com.redefocus.common.shared.permissions.user.data.User;
+import com.redefocus.factionscaribe.user.data.CaribeUser;
+import org.bukkit.command.CommandSender;
+
+public class VanishCommand extends CustomCommand {
+    public VanishCommand() {
+        super(
+                "v",
+                CommandRestriction.IN_GAME,
+                GroupNames.MODERATOR
+        );
+    }
+
+    @Override
+    public void onCommand(CommandSender commandSender, User user, String[] args) {
+        CaribeUser caribeUser = (CaribeUser) user;
+
+        caribeUser.setInvisible(!caribeUser.isInvisible());
+
+        commandSender.sendMessage("§aO modo invisível foi " + (caribeUser.isInvisible() ? "ativado." : "desativado."));
+    }
+}
