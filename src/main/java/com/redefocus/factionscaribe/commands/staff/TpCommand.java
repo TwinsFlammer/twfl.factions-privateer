1fef7a50aedd6b6c70a8e439178699c7c5c2d3d0
Gutyerrez <srgutyerrez@gmail.com>
2020-04-20 12:56:17 -0300

    Create TpCommand.java

---
+++ src/main/java/com/redefocus/factionscaribe/commands/staff/TpCommand.java
@@ -0,0 +1,64 @@
+package com.redefocus.factionscaribe.commands.staff;
+
+import com.redefocus.api.spigot.SpigotAPI;
+import com.redefocus.api.spigot.commands.CustomCommand;
+import com.redefocus.api.spigot.commands.enums.CommandRestriction;
+import com.redefocus.api.spigot.teleport.data.TeleportRequest;
+import com.redefocus.common.shared.permissions.group.GroupNames;
+import com.redefocus.common.shared.permissions.user.data.User;
+import com.redefocus.common.shared.server.data.Server;
+import com.redefocus.factionscaribe.FactionsCaribe;
+import com.redefocus.factionscaribe.user.data.CaribeUser;
+import org.bukkit.command.CommandSender;
+
+public class TpCommand extends CustomCommand {
+    public TpCommand() {
+        super(
+                "tp",
+                CommandRestriction.IN_GAME,
+                GroupNames.MODERATOR
+        );
+    }
+
+    @Override
+    public void onCommand(CommandSender commandSender, User user, String[] args) {
+        if (args.length != 1) {
+            commandSender.sendMessage("§cUtilize /tp <jogador>.");
+            return;
+        }
+
+        String targetName = args[0];
+
+        CaribeUser caribeUser = FactionsCaribe.getInstance().getCaribeUserFactory().getUser(user.getId());
+        CaribeUser targetUser = FactionsCaribe.getInstance().getCaribeUserFactory().getUser(targetName);
+
+        if (targetUser == null) {
+            commandSender.sendMessage("§cEste usuário não existe.");
+            return;
+        }
+
+        if (!targetUser.isOnline()) {
+            commandSender.sendMessage("§cEste usuário não está online.");
+            return;
+        }
+
+        Server server = SpigotAPI.getCurrentServer();
+
+        TeleportRequest teleportRequest = new TeleportRequest(
+                user.getId(),
+                targetUser.getId(),
+                null,
+                server.getId(),
+                0L
+        );
+
+        teleportRequest.start();
+
+        commandSender.sendMessage(
+                String.format(
+                        "§aTeleportando-se para %s§a.",
+                        caribeUser.getPrefix() + caribeUser.getDisplayName()
+                )
+        );
+    }
+}
