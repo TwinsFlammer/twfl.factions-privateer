359cb0b4ba7bd4cb33fc5eb365a02c3ba14df6f9
oNospher <fealtyservers@gmail.com>
2020-04-21 09:50:09 -0300

    Update Home.java

--- src/main/java/com/redefocus/factionscaribe/home/data/Home.java
+++ src/main/java/com/redefocus/factionscaribe/home/data/Home.java
@@ -1,10 +1,16 @@
 package com.redefocus.factionscaribe.home.data;
 
+import com.redefocus.api.spigot.util.serialize.LocationSerialize;
 import com.redefocus.factionscaribe.home.enums.HomeState;
+import com.redefocus.factionscaribe.user.data.CaribeUser;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.bukkit.Chunk;
 import org.bukkit.Location;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * @author oNospher
@@ -32,5 +38,25 @@ public class Home {
 
     public Boolean isPrivate() {
         return !isPublic();
+    }
+
+    public Boolean canTeleport(CaribeUser caribeUser) {
+        Chunk chunk = location.getChunk();
+
+    //    Faction at = BoardColl.get().getFactionAt(PS.valueOf(chunk));
+
+    //    if (at == null || at.isNone()) return true;
+        return false;
+    }
+
+    public static Home toHome(ResultSet resultSet) throws SQLException {
+        return new Home(
+                resultSet.getInt("id"),
+                resultSet.getInt("user_id"),
+                resultSet.getString("name"),
+                resultSet.getInt("server_id"),
+                LocationSerialize.toLocation(resultSet.getString("location")),
+                HomeState.valueOf(resultSet.getString("state"))
+        );
     }
 }
