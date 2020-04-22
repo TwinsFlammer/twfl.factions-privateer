3ca41b6423731e2ccfcae433bf80e723bfb8a505
Gutyerrez <srgutyerrez@gmail.com>
2020-04-22 08:54:43 -0300

    Update ChatManager.java

---
+++ src/main/java/com/redefocus/factionscaribe/chat/manager/ChatManager.java
@@ -0,0 +1,10 @@
+package com.redefocus.factionscaribe.chat.manager;
+
+import org.json.simple.JSONObject;
+
+/**
+ * @author SrGutyerrez
+ */
+public class ChatManager {
+    public static JSONObject
+}
