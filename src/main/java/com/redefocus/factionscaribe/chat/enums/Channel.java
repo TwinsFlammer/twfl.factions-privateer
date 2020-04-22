c25399d75fa74394b9376d994dc9881fabd87596
Gutyerrez <srgutyerrez@gmail.com>
2020-04-22 15:10:46 -0300

    Update Channel.java

--- src/main/java/com/redefocus/factionscaribe/chat/enums/Channel.java
+++ src/main/java/com/redefocus/factionscaribe/chat/enums/Channel.java
@@ -1,27 +1,42 @@
 package com.redefocus.factionscaribe.chat.enums;
 
 import com.redefocus.api.spigot.SpigotAPI;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.bukkit.ChatColor;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author SrGutyerrez
  */
 @RequiredArgsConstructor
+@Getter
 public enum Channel {
     LOCAL(
-            "L"
+            "L",
+            ChatColor.YELLOW,
+            TimeUnit.SECONDS.toMillis(3)
     ),
     GLOBAL(
-            "G"
+            "G",
+            ChatColor.GRAY,
+            TimeUnit.SECONDS.toMillis(10)
     ),
     FACTION(
-            ""
+            "",
+            ChatColor.AQUA,
+            TimeUnit.SECONDS.toMillis(3)
     ),
     ALLY(
-            "A"
+            "§e[§a%s§e]",
+            ChatColor.WHITE,
+            TimeUnit.SECONDS.toMillis(3)
     );
 
     private final String prefix;
+    private final ChatColor color;
+    private final Long delay;
 
     public String getHKey() {
         return "chat_" + SpigotAPI.getRootServerId() + "_" + this.name();
