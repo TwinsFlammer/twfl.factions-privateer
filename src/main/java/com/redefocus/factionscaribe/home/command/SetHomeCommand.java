cb080bc0b9a80039534f745ff8c8a3f36ef0684f
oNospher <fealtyservers@gmail.com>
2020-04-22 13:27:55 -0300

    Update SetHomeCommand.java

--- src/main/java/com/redefocus/factionscaribe/home/command/SetHomeCommand.java
+++ src/main/java/com/redefocus/factionscaribe/home/command/SetHomeCommand.java
@@ -10,6 +10,7 @@ import com.redefocus.factionscaribe.FactionsCaribe;
 import com.redefocus.factionscaribe.home.dao.HomeDao;
 import com.redefocus.factionscaribe.home.data.Home;
 import com.redefocus.factionscaribe.user.data.CaribeUser;
+import com.redefocus.factionscaribe.util.FactionUtil;
 import org.bukkit.command.CommandSender;
 
 import java.util.HashMap;
@@ -18,7 +19,9 @@ import java.util.HashMap;
  * @author oNospher
  **/
 public class SetHomeCommand extends CustomCommand {
+
     private final Integer MAX_HOME_NAME_LENGHT = 32;
+    private final String WORLD_PERMITTED = "world";
 
     public SetHomeCommand() {
         super(
@@ -37,10 +40,22 @@ public class SetHomeCommand extends CustomCommand {
         CaribeUser caribeUser = FactionsCaribe.getInstance().getCaribeUserFactory().getUser(user.getId());
         String name = args[0];
 
+        if(!caribeUser.getWorld().getName().equalsIgnoreCase(this.WORLD_PERMITTED)) {
+            commandSender.sendMessage(
+                    "§cVocê não pode definir home nesse lugar."
+            );
+        }
+
+        if(!FactionUtil.isAllowed()) {
+            commandSender.sendMessage(
+                    "§cVocê não pode definir home nesse lugar."
+            );
+        }
+
         if(name.length() > this.MAX_HOME_NAME_LENGHT) {
             commandSender.sendMessage(
                     String.format(
-                            "§cO nome da home tem que ter no máximo %d.",
+                            "§cO nome da home tem que ter no máximo %d caracteres .",
                             this.MAX_HOME_NAME_LENGHT
                     )
             );
