22e74f2088e79fd1f8de1cb85b069db85d05348e
Gutyerrez <srgutyerrez@gmail.com>
2020-04-23 11:24:21 -0300

    Update GlobalCommand.java

--- src/main/java/com/redefocus/factionscaribe/chat/commands/GlobalCommand.java
+++ src/main/java/com/redefocus/factionscaribe/chat/commands/GlobalCommand.java
@@ -1,11 +1,17 @@
 package com.redefocus.factionscaribe.chat.commands;
 
+import com.redefocus.api.spigot.SpigotAPI;
 import com.redefocus.api.spigot.commands.CustomCommand;
 import com.redefocus.api.spigot.commands.enums.CommandRestriction;
 import com.redefocus.common.shared.cooldown.manager.CooldownManager;
 import com.redefocus.common.shared.permissions.group.GroupNames;
 import com.redefocus.common.shared.permissions.user.data.User;
+import com.redefocus.common.shared.util.Helper;
 import com.redefocus.common.shared.util.TimeFormatter;
+import com.redefocus.factionscaribe.FactionsCaribe;
+import com.redefocus.factionscaribe.chat.component.ChatComponent;
+import com.redefocus.factionscaribe.chat.enums.Channel;
+import com.redefocus.factionscaribe.user.data.CaribeUser;
 import org.bukkit.command.CommandSender;
 
 /**
@@ -25,11 +31,28 @@ public class GlobalCommand extends CustomCommand {
         if (CooldownManager.inCooldown(user, "CHAT_GLOBAL")) {
             commandSender.sendMessage(
                     String.format(
-                            "",
-                            TimeFormatter.formatMinimized(CooldownManager.getRemainingTime(user, "CHAT_GLOBAL"))
+                            "§cAguarde %s para falar no chat novamente",
+                            TimeFormatter.formatExtended(CooldownManager.getRemainingTime(user, "CHAT_GLOBAL"))
                     )
             );
             return;
         }
+
+        CaribeUser caribeUser = FactionsCaribe.getInstance().getCaribeUserFactory().getUser(user.getId());
+
+        String message = Helper.toMessage(args);
+
+        ChatComponent chatComponent = new ChatComponent(Channel.GLOBAL, caribeUser, message) {
+            @Override
+            public void send(CaribeUser caribeUser) {
+                super.send(caribeUser);
+            }
+        };
+
+        SpigotAPI.getUsers().forEach(user1 -> {
+            CaribeUser caribeUser1 = FactionsCaribe.getInstance().getCaribeUserFactory().getUser(user1.getId());
+
+            chatComponent.send(caribeUser1);
+        });
     }
 }
