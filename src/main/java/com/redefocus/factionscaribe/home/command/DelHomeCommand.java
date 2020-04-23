ba0068cbd7f88fb55c24e104824a69c29b58797c
oNospher <fealtyservers@gmail.com>
2020-04-23 13:20:30 -0300

    Create DelHomeCommand.java

---
+++ src/main/java/com/redefocus/factionscaribe/home/command/DelHomeCommand.java
@@ -0,0 +1,34 @@
+package com.redefocus.factionscaribe.home.command;
+
+import com.redefocus.api.spigot.commands.CustomCommand;
+import com.redefocus.api.spigot.commands.enums.CommandRestriction;
+import com.redefocus.common.shared.permissions.group.GroupNames;
+import com.redefocus.common.shared.permissions.user.data.User;
+import com.redefocus.factionscaribe.FactionsCaribe;
+import com.redefocus.factionscaribe.user.data.CaribeUser;
+import org.bukkit.command.CommandSender;
+
+/**
+ * @author oNospher
+ **/
+public class DelHomeCommand extends CustomCommand {
+
+    public DelHomeCommand() {
+        super(
+                "delhome",
+                CommandRestriction.IN_GAME,
+                GroupNames.DEFAULT
+        );
+    }
+
+    @Override
+    public void onCommand(CommandSender commandSender, User user, String[] args) {
+        if(args.length != 1) {
+            commandSender.sendMessage("§cUtilize /delhome <home>");
+            return;
+        }
+        CaribeUser caribeUser = FactionsCaribe.getInstance().getCaribeUserFactory().getUser(user.getId());
+        String name = args[0];
+
+    }
+}
