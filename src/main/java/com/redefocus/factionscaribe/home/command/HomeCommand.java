bcde07f8a670db138d8a29451989986812176b3e
oNospher <fealtyservers@gmail.com>
2020-04-24 08:21:59 -0300

    Create HomeCommand.java

---
+++ src/main/java/com/redefocus/factionscaribe/home/command/HomeCommand.java
@@ -0,0 +1,70 @@
+package com.redefocus.factionscaribe.home.command;
+
+import com.redefocus.api.spigot.commands.CustomCommand;
+import com.redefocus.api.spigot.commands.enums.CommandRestriction;
+import com.redefocus.common.shared.permissions.group.GroupNames;
+import com.redefocus.common.shared.permissions.user.data.User;
+import com.redefocus.factionscaribe.FactionsCaribe;
+import com.redefocus.factionscaribe.chat.component.ChatComponent;
+import com.redefocus.factionscaribe.chat.enums.Channel;
+import com.redefocus.factionscaribe.home.component.HomeComponent;
+import com.redefocus.factionscaribe.user.data.CaribeUser;
+import org.bukkit.command.CommandSender;
+
+/**
+ * @author oNospher
+ **/
+public class HomeCommand extends CustomCommand {
+
+    public HomeCommand() {
+        super(
+                "home",
+                CommandRestriction.IN_GAME,
+                GroupNames.DEFAULT,
+                new String[]{
+                        "homes"
+                }
+        );
+    }
+
+    @Override
+    public void onCommand(CommandSender commandSender, User user, String[] args) {
+        CaribeUser caribeUser = FactionsCaribe.getInstance().getCaribeUserFactory().getUser(user.getId());
+        switch (args.length) {
+            case 1: {
+                String argument = args[0];
+                if(argument.contains(":")) {
+                    String owner = argument.split(":")[0];
+                    CaribeUser caribeTarget = FactionsCaribe.getInstance().getCaribeUserFactory().getUser(owner);
+                    if(caribeTarget == null) {
+                        commandSender.sendMessage("§cEste usuário não existe.");
+                        return;
+                    }
+                    try {
+
+                    } catch (Exception exception) {
+                        HomeComponent homeComponent = new HomeComponent(caribeTarget) {
+                            @Override
+                            public void send(CaribeUser caribeUser) {
+                                super.send(caribeUser);
+                            }
+                        };
+                        homeComponent.send(caribeUser);
+                    }
+
+                } else {
+
+                }
+            }
+            default: {
+                HomeComponent homeComponent = new HomeComponent(caribeUser) {
+                    @Override
+                    public void send(CaribeUser caribeUser) {
+                        super.send(caribeUser);
+                    }
+                };
+                homeComponent.send(caribeUser);
+            }
+        }
+    }
+}
