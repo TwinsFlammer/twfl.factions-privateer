a3881905c780652a93ff6849490fb196f98d175c
oNospher <fealtyservers@gmail.com>
2020-04-23 22:06:17 -0300

    Update PublicCommand.java

--- src/main/java/com/redefocus/factionscaribe/home/command/PublicCommand.java
+++ src/main/java/com/redefocus/factionscaribe/home/command/PublicCommand.java
@@ -31,21 +31,29 @@ public class PublicCommand extends CustomCommand {
             commandSender.sendMessage("§cUtilize /publica <home>");
             return;
         }
+
         CaribeUser caribeUser = FactionsCaribe.getInstance().getCaribeUserFactory().getUser(user.getId());
+
         String name = args[0];
         Home home = caribeUser.getHomeExact(name);
+
         if(home == null) {
             commandSender.sendMessage("§cEsta home não existe.");
             return;
         }
+
         if(home.isPublic()) {
             commandSender.sendMessage("§cEsta home já é pública.");
             return;
         }
+
         HomeDao<Home> homeDao = new HomeDao<>();
+
         HashMap<Object, Object> hashMap = new HashMap<>();
         hashMap.put("state", Home.State.PUBLIC.toString());
+
         homeDao.update(hashMap, "id", home.getId());
+
         commandSender.sendMessage(
                 String.format(
                        "§aPública \"%s\" adicionada com sucesso.",
