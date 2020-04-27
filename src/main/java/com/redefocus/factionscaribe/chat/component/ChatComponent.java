50680a0f35490f7a2d2a41ac2dd13350df842d45
Gutyerrez <srgutyerrez@gmail.com>
2020-04-27 20:53:34 -0300

    Update ChatComponent.java

--- src/main/java/com/redefocus/factionscaribe/chat/component/ChatComponent.java
+++ src/main/java/com/redefocus/factionscaribe/chat/component/ChatComponent.java
@@ -1,5 +1,7 @@
 package com.redefocus.factionscaribe.chat.component;
 
+import com.massivecraft.factions.entity.Faction;
+import com.massivecraft.factions.entity.MPlayer;
 import com.redefocus.api.spigot.util.jsontext.data.JSONText;
 import com.redefocus.factionscaribe.chat.enums.Channel;
 import com.redefocus.factionscaribe.user.data.CaribeUser;
@@ -13,11 +15,15 @@ public abstract class ChatComponent {
     public ChatComponent(Channel channel, CaribeUser user, String message) {
         JSONText jsonText;
 
+        MPlayer mPlayer = MPlayer.get(user.getUniqueId());
+
+        Faction faction = mPlayer.getFaction();
+
         switch (channel) {
             case GLOBAL:
             case LOCAL: {
                 jsonText = new JSONText()
-                        .text(channel.getColor() + "[" + channel.getPrefix() + "]")
+                        .text(channel.getColor() + "[" + channel.getPrefix() + "] ")
                         .next()
                         .text(user.getPrefix())
                         .next()
@@ -25,15 +31,15 @@ public abstract class ChatComponent {
                         .next()
                         .text(user.getDisplayName())
                         .hoverText(
-                                "§6Nick: §f" +
+                                "§6Nick: §f" + user.getPrefix() + user.getDisplayName() +
                                 "\n" +
-                                "§6Facção: §f" +
+                                "§6Facção: §f" + faction.getTag() + "- " + faction.getName() +
                                 "\n" +
-                                "§6KDR: §f" +
+                                "§6KDR: §f" + mPlayer.getKdrRounded() +
                                 "\n" +
-                                "§6Poder: §f" +
+                                "§6Poder: §f" + mPlayer.getPowerRounded() + "/" + mPlayer.getPowerMaxRounded() +
                                 "\n" +
-                                "§6Coins: §f"
+                                "§6Coins: §f" + 0.0
                         )
                         .next()
                         .text(channel.getColor() + ": ")
