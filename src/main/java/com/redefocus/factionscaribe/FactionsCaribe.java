459c9444cc3f9a90a05c2aa3cabe218824278385
Gutyerrez <srgutyerrez@gmail.com>
2020-04-28 18:03:49 -0300

    Update FactionsCaribe.java

--- src/main/java/com/redefocus/factionscaribe/FactionsCaribe.java
+++ src/main/java/com/redefocus/factionscaribe/FactionsCaribe.java
@@ -2,6 +2,7 @@ package com.redefocus.factionscaribe;
 
 import com.redefocus.api.spigot.FocusPlugin;
 import com.redefocus.factionscaribe.manager.StartManager;
+import com.redefocus.factionscaribe.mcmmo.mcMMO;
 import com.redefocus.factionscaribe.user.data.CaribeUser;
 import com.redefocus.factionscaribe.user.factory.CaribeUserFactory;
 import lombok.Getter;
@@ -23,15 +24,21 @@ public class FactionsCaribe extends FocusPlugin {
     @Getter
     private static final Long startTime = System.currentTimeMillis();
 
+    private mcMMO mcMMO;
+
     @Override
     public void onEnablePlugin() {
         new StartManager();
 
         this.caribeUserFactory = new CaribeUserFactory<>();
+
+        this.mcMMO = new mcMMO();
+
+        this.mcMMO.onEnable();
     }
 
     @Override
     public void onDisablePlugin() {
-
+        this.mcMMO.onDisable();
     }
 }
