76f7c320d05f0d237e5256b62f2b8c1592d3b038
Gutyerrez <srgutyerrez@gmail.com>
2020-04-27 21:34:48 -0300

    Update GlobalCommand.java

--- src/main/java/com/redefocus/factionscaribe/chat/commands/GlobalCommand.java
+++ src/main/java/com/redefocus/factionscaribe/chat/commands/GlobalCommand.java
@@ -3,6 +3,7 @@ package com.redefocus.factionscaribe.chat.commands;
 import com.redefocus.api.spigot.SpigotAPI;
 import com.redefocus.api.spigot.commands.CustomCommand;
 import com.redefocus.api.spigot.commands.enums.CommandRestriction;
+import com.redefocus.api.spigot.util.action.data.CustomAction;
 import com.redefocus.common.shared.cooldown.manager.CooldownManager;
 import com.redefocus.common.shared.permissions.group.GroupNames;
 import com.redefocus.common.shared.permissions.user.data.User;
@@ -13,11 +14,16 @@ import com.redefocus.factionscaribe.chat.component.ChatComponent;
 import com.redefocus.factionscaribe.chat.enums.Channel;
 import com.redefocus.factionscaribe.user.data.CaribeUser;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author SrGutyerrez
  */
 public class GlobalCommand extends CustomCommand {
+    private static final String OBJECT_NAME = "CHAT_GLOBAL";
+
     public GlobalCommand() {
         super(
                 "g",
@@ -28,13 +34,18 @@ public class GlobalCommand extends CustomCommand {
 
     @Override
     public void onCommand(CommandSender commandSender, User user, String[] args) {
-        if (CooldownManager.inCooldown(user, "CHAT_GLOBAL")) {
-            commandSender.sendMessage(
-                    String.format(
-                            "§cAguarde %s para falar no chat novamente",
-                            TimeFormatter.formatExtended(CooldownManager.getRemainingTime(user, "CHAT_GLOBAL"))
+        Player player = (Player) commandSender;
+
+        if (CooldownManager.inCooldown(user, GlobalCommand.OBJECT_NAME)) {
+            new CustomAction()
+                    .text(
+                            String.format(
+                                    "§cAguarde %s para falar no chat novamente",
+                                    TimeFormatter.formatExtended(CooldownManager.getRemainingTime(user, GlobalCommand.OBJECT_NAME))
+                            )
                     )
-            );
+                    .getSpigot()
+                    .send(player);
             return;
         }
 
@@ -42,6 +53,27 @@ public class GlobalCommand extends CustomCommand {
             commandSender.sendMessage("§cUtilize /g <mensagem>.");
             return;
         }
+
+        if ((user.getFirstLogin() + TimeUnit.HOURS.toMillis(5)) >= System.currentTimeMillis()) {
+            new CustomAction()
+                    .text(
+                            String.format(
+                                    "§cVocê deve aguardar %s para poder falar no chat global.",
+                                    TimeFormatter.formatExtended(
+                                            (user.getFirstLogin() + TimeUnit.HOURS.toMillis(5)) - System.currentTimeMillis()
+                                    )
+                            )
+                    )
+                    .getSpigot()
+                    .send(player);
+            return;
+        }
+
+        CooldownManager.startCooldown(
+                user,
+                TimeUnit.SECONDS.toMillis(15),
+                GlobalCommand.OBJECT_NAME
+        );
 
         CaribeUser caribeUser = FactionsCaribe.getInstance().getCaribeUserFactory().getUser(user.getId());
 
