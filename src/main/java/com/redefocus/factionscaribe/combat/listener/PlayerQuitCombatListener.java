5b6bd36e34af92ca759f64a648a7a3f534e0ba3d
oNospher <fealtyservers@gmail.com>
2020-04-29 11:06:02 -0300

    Update PlayerQuitCombatListener.java

--- src/main/java/com/redefocus/factionscaribe/combat/listener/PlayerQuitCombatListener.java
+++ src/main/java/com/redefocus/factionscaribe/combat/listener/PlayerQuitCombatListener.java
@@ -1,5 +1,10 @@
 package com.redefocus.factionscaribe.combat.listener;
 
+import com.redefocus.api.spigot.SpigotAPI;
+import com.redefocus.factionscaribe.FactionsCaribe;
+import com.redefocus.factionscaribe.user.data.CaribeUser;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -11,6 +16,13 @@ public class PlayerQuitCombatListener implements Listener {
 
     @EventHandler
     public void onQuit(PlayerQuitEvent event) {
-
+        if(SpigotAPI.getCurrentServer().isRestarting()) return;
+        Player player = event.getPlayer();
+        CaribeUser caribeUser = FactionsCaribe.getInstance().getCaribeUserFactory().getUser(player.getUniqueId());
+        if(caribeUser.inCombat()) {
+            player.setHealth(0.0D);
+            caribeUser.setCombatDuration(0L);
+            Bukkit.broadcastMessage(caribeUser.getPrefix() + player.getName() + " desconectou em combate, que vergonha..");
+        }
     }
 }
