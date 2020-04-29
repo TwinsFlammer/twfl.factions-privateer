fb505dee5d5ecac96ed97827a1468839ef671739
oNospher <fealtyservers@gmail.com>
2020-04-29 10:18:19 -0300

    Create PlayerQuitCombatListener.java

---
+++ src/main/java/com/redefocus/factionscaribe/combat/listener/PlayerQuitCombatListener.java
@@ -0,0 +1,16 @@
+package com.redefocus.factionscaribe.combat.listener;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+/**
+ * @author oNospher
+ **/
+public class PlayerQuitCombatListener implements Listener {
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent event) {
+
+    }
+}
