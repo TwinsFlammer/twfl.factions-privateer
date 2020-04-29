311cccafb658a63fe69f5ca375fd312d4fae8e26
oNospher <fealtyservers@gmail.com>
2020-04-29 11:52:51 -0300

    Update PlayerCommandCombatListener.java

--- src/main/java/com/redefocus/factionscaribe/combat/listener/PlayerCommandCombatListener.java
+++ src/main/java/com/redefocus/factionscaribe/combat/listener/PlayerCommandCombatListener.java
@@ -1,6 +1,13 @@
 package com.redefocus.factionscaribe.combat.listener;
 
+import com.redefocus.factionscaribe.FactionsCaribe;
+import com.redefocus.factionscaribe.user.data.CaribeUser;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+
+import java.util.Arrays;
 
 /**
  * @author oNospher
@@ -15,6 +22,21 @@ public class PlayerCommandCombatListener implements Listener {
             "/a",
             "/report"
     };
+
+    protected final String NOT_ALLOWED_COMMAND_IN_COMBAT = "§cO uso de comandos em combate está desabilitado.";
+
+    @EventHandler
+    public void onCommand(PlayerCommandPreprocessEvent event) {
+        Player player = event.getPlayer();
+        String message = event.getMessage();
+
+        CaribeUser caribeUser = FactionsCaribe.getInstance().getCaribeUserFactory().getUser(player.getUniqueId());
+        if(caribeUser.inCombat()) {
+            if(Arrays.asList(allowed).contains(message.toLowerCase())) return;
+            event.setCancelled(true);
+            player.sendMessage(this.NOT_ALLOWED_COMMAND_IN_COMBAT);
+        }
+    }
 
 
 }
