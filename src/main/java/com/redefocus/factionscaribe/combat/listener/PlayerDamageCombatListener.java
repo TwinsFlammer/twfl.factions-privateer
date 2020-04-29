a5316b121ab72ee9d54c0853c080a4f5746fe1d3
oNospher <fealtyservers@gmail.com>
2020-04-29 09:55:38 -0300

    Update PlayerDamageCombatListener.java

--- src/main/java/com/redefocus/factionscaribe/combat/listener/PlayerDamageCombatListener.java
+++ src/main/java/com/redefocus/factionscaribe/combat/listener/PlayerDamageCombatListener.java
@@ -1,9 +1,33 @@
 package com.redefocus.factionscaribe.combat.listener;
 
+import com.redefocus.factionscaribe.FactionsCaribe;
+import com.redefocus.factionscaribe.user.data.CaribeUser;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
 /**
  * @author oNospher
  **/
 public class PlayerDamageCombatListener implements Listener {
+
+    @EventHandler(ignoreCancelled = true)
+    public void onDamage(EntityDamageByEntityEvent event) {
+        if(event.getEntity() instanceof Player && event.getDamager() instanceof Player) {
+
+            if(event.isCancelled() || event.getDamager() == null) return;
+
+            Player player = (Player) event.getEntity();
+            Player damager = (Player) event.getDamager();
+
+            if(player.hasMetadata("NPC") || damager.hasMetadata("NPC")) return;
+
+            CaribeUser caribeUser = FactionsCaribe.getInstance().getCaribeUserFactory().getUser(player.getUniqueId());
+            CaribeUser caribeDamager = FactionsCaribe.getInstance().getCaribeUserFactory().getUser(damager.getUniqueId());
+
+            caribeUser.setCombat(caribeDamager);
+            caribeDamager.setCombat(caribeUser);
+        }
+    }
 }
