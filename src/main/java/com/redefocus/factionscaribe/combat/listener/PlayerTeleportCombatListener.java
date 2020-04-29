fb15871e4becae5685c650ffc8f09116313986e5
oNospher <fealtyservers@gmail.com>
2020-04-29 11:11:36 -0300

    Create PlayerTeleportCombatListener.java

---
+++ src/main/java/com/redefocus/factionscaribe/combat/listener/PlayerTeleportCombatListener.java
@@ -0,0 +1,11 @@
+package com.redefocus.factionscaribe.combat.listener;
+
+import org.bukkit.event.Listener;
+
+/**
+ * @author oNospher
+ **/
+public class PlayerTeleportCombatListener implements Listener {
+
+
+}
