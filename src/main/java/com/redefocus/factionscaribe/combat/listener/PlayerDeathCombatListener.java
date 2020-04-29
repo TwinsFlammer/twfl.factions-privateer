40efd24c3da55f0fbfc53b54982f2f375a37933f
oNospher <fealtyservers@gmail.com>
2020-04-29 11:20:50 -0300

    Create PlayerDeathCombatListener.java

---
+++ src/main/java/com/redefocus/factionscaribe/combat/listener/PlayerDeathCombatListener.java
@@ -0,0 +1,22 @@
+package com.redefocus.factionscaribe.combat.listener;
+
+import com.redefocus.factionscaribe.FactionsCaribe;
+import com.redefocus.factionscaribe.user.data.CaribeUser;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+/**
+ * @author oNospher
+ **/
+public class PlayerDeathCombatListener implements Listener {
+
+    @EventHandler
+    public void onDeath(PlayerDeathEvent event) {
+        Player player = event.getEntity();
+        CaribeUser caribeUser = FactionsCaribe.getInstance().getCaribeUserFactory().getUser(player.getUniqueId());
+
+        caribeUser.setCombatDuration(0L);
+    }
+}
