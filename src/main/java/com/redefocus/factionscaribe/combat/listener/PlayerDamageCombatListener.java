ebcb62b2b7bdb6489bebcd458088f7224baf23a7
oNospher <fealtyservers@gmail.com>
2020-04-29 09:37:43 -0300

    Create PlayerDamageCombatListener.java

---
+++ src/main/java/com/redefocus/factionscaribe/combat/listener/PlayerDamageCombatListener.java
@@ -0,0 +1,9 @@
+package com.redefocus.factionscaribe.combat.listener;
+
+import org.bukkit.event.Listener;
+
+/**
+ * @author oNospher
+ **/
+public class PlayerDamageCombatListener implements Listener {
+}
