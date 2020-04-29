7a09b8dca88f1025486065cd7f51ca83e5839fe8
oNospher <fealtyservers@gmail.com>
2020-04-29 11:28:47 -0300

    Create PlayerCommandCombatListener.java

---
+++ src/main/java/com/redefocus/factionscaribe/combat/listener/PlayerCommandCombatListener.java
@@ -0,0 +1,20 @@
+package com.redefocus.factionscaribe.combat.listener;
+
+import org.bukkit.event.Listener;
+
+/**
+ * @author oNospher
+ **/
+public class PlayerCommandCombatListener implements Listener {
+
+    private final String[] allowed = new String[]{
+            "/g",
+            "/c",
+            "/.",
+            "/tell",
+            "/a",
+            "/report"
+    };
+
+
+}
