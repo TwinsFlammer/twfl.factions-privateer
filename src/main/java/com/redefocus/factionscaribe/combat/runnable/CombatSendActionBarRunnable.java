ef42369f1a33dd6dbe96c75efc317a496647f14f
oNospher <fealtyservers@gmail.com>
2020-04-28 22:38:01 -0300

    Update CombatSendActionBarRunnable.java

--- src/main/java/com/redefocus/factionscaribe/combat/runnable/CombatSendActionBarRunnable.java
+++ src/main/java/com/redefocus/factionscaribe/combat/runnable/CombatSendActionBarRunnable.java
@@ -2,25 +2,35 @@ package com.redefocus.factionscaribe.combat.runnable;
 
 import com.redefocus.api.spigot.SpigotAPI;
 import com.redefocus.factionscaribe.FactionsCaribe;
-import com.redefocus.factionscaribe.user.data.CaribeUser;
 
 import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author oNospher
  **/
 public class CombatSendActionBarRunnable implements Runnable {
 
-    private final String LEAVE_COMBAT_MESSAGE = "";
+    private final String IN_COMBAT_MESSAGE = "§c%s segundos para sair de combate.";
+    private final String LEAVE_COMBAT_MESSAGE = "§aVocê não está mais em combate, pode deslogar.";
 
     @Override
     public void run() {
         SpigotAPI.getUsers().stream()
                 .filter(Objects::nonNull)
+                .map(user -> FactionsCaribe.getInstance().getCaribeUserFactory().getUser(user.getId()))
+                .filter(user -> user.getCombatDuration() != 0)
                 .forEach(user -> {
-                    CaribeUser caribeUser = FactionsCaribe.getInstance().getCaribeUserFactory().getUser(user.getId());
-                    if(caribeUser.inCombat()) {
-
+                    if(user.inCombat()) {
+                        user.sendAction(
+                                String.format(
+                                        this.IN_COMBAT_MESSAGE,
+                                        (int) TimeUnit.MILLISECONDS.toSeconds(user.getCombatTime())
+                                )
+                        );
+                    } else {
+                        user.setCombatDuration(0L);
+                        user.sendMessage(this.LEAVE_COMBAT_MESSAGE);
                     }
                 });
     }
