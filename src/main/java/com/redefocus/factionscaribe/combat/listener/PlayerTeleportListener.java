a75195b1bc64940ed1668c7cfe60de280807f223
Gutyerrez <srgutyerrez@gmail.com>
2020-05-04 21:20:43 -0300

    Create PlayerTeleportListener.java

---
+++ src/main/java/com/redefocus/factionscaribe/combat/listener/PlayerTeleportListener.java
@@ -0,0 +1,28 @@
+package com.redefocus.factionscaribe.combat.listener;
+
+import com.redefocus.factionscaribe.FactionsCaribe;
+import com.redefocus.factionscaribe.user.data.CaribeUser;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerTeleportEvent;
+
+/**
+ * @author SrGutyerrez
+ */
+public class PlayerTeleportListener implements Listener {
+    @EventHandler
+    public void onTeleport(PlayerTeleportEvent event) {
+        Player player = event.getPlayer();
+        PlayerTeleportEvent.TeleportCause teleportCause = event.getCause();
+
+        CaribeUser caribeUser = FactionsCaribe.getInstance().getCaribeUserFactory().getUser(player.getUniqueId());
+
+        if (teleportCause == PlayerTeleportEvent.TeleportCause.ENDER_PEARL && caribeUser.inCombat()) {
+            Location from = event.getFrom();
+
+            player.teleport(from);
+        }
+    }
+}
