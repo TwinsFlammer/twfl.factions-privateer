84c4d5bce86324b348e5909fd3f8c791160894d4
Gutyerrez <srgutyerrez@gmail.com>
2020-05-04 21:19:53 -0300

    Create MoneyRemoveCommand.java

---
+++ src/main/java/com/redefocus/factionscaribe/economy/command/arguments/MoneyRemoveCommand.java
@@ -0,0 +1,68 @@
+package com.redefocus.factionscaribe.economy.command.arguments;
+
+import com.redefocus.api.spigot.commands.CustomArgumentCommand;
+import com.redefocus.common.shared.permissions.user.data.User;
+import com.redefocus.common.shared.permissions.user.manager.UserManager;
+import com.redefocus.common.shared.util.Helper;
+import com.redefocus.factionscaribe.FactionsCaribe;
+import com.redefocus.factionscaribe.economy.command.MoneyCommand;
+import com.redefocus.factionscaribe.economy.manager.EconomyManager;
+import com.redefocus.factionscaribe.user.data.CaribeUser;
+import org.bukkit.command.CommandSender;
+
+/**
+ * @author SrGutyerrez
+ */
+public class MoneyRemoveCommand<C extends MoneyCommand> extends CustomArgumentCommand<C> {
+    public MoneyRemoveCommand() {
+        super(
+                0,
+                "remove"
+        );
+    }
+
+    @Override
+    public void onCommand(CommandSender commandSender, User user, String[] args) {
+        if (args.length == 2) {
+            User user1 = UserManager.getUser(args[0]);
+
+            if (user1 == null) {
+                commandSender.sendMessage("§cEste usuário não existe.");
+                return;
+            }
+
+            CaribeUser caribeUser = FactionsCaribe.getInstance().getCaribeUserFactory().getUser(user1.getId());
+
+            if (caribeUser == null) {
+                commandSender.sendMessage("§cEste usuário nunca entrou neste servidor.");
+                return;
+            }
+
+            Double money = Helper.isDouble(args[1]) ? Double.parseDouble(args[1]) : null;
+
+            if (money == null || money.isNaN() || money <= 0) {
+                commandSender.sendMessage("§cVocê inseriu um valor inválido.");
+                return;
+            }
+
+            if (caribeUser.getMoney() < money) {
+                commandSender.sendMessage("§cEste usuário não possui saldo suficiente.");
+                return;
+            }
+
+            caribeUser.withdraw(money);
+
+            commandSender.sendMessage(
+                    String.format(
+                            "§eO saldo de %s §efoi atualizado para %s.",
+                            caribeUser.getPrefix() + caribeUser.getDisplayName(),
+                            EconomyManager.format(
+                                    caribeUser.getMoney()
+                            )
+                    )
+            );
+        } else {
+            commandSender.sendMessage("§cUtilize /money remove <usuário> <quantia>.");
+        }
+    }
+}
