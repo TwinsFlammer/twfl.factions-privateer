3fc8c2437ff6d932e67bb283acc41397ad2e1040
Gutyerrez <srgutyerrez@gmail.com>
2020-05-07 00:34:22 -0300

    Update UserTeleportListener.java

--- src/main/java/com/redefocus/factionscaribe/listeners/general/UserTeleportListener.java
+++ src/main/java/com/redefocus/factionscaribe/listeners/general/UserTeleportListener.java
@@ -1,16 +1,17 @@
-package com.redefocus.factionscaribe.combat.listener;
+package com.redefocus.factionscaribe.listeners.general;
 
 import com.redefocus.api.spigot.teleport.data.TeleportRequest;
 import com.redefocus.api.spigot.teleport.event.UserTeleportEvent;
+import com.redefocus.api.spigot.util.serialize.LocationSerialize;
 import com.redefocus.factionscaribe.FactionsCaribe;
 import com.redefocus.factionscaribe.user.data.CaribeUser;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 /**
- * @author oNospher
- **/
-public class UserTeleportCombatListener implements Listener {
+ * @author SrGutyerrez
+ */
+public class UserTeleportListener implements Listener {
     @EventHandler
     public void onTeleport(UserTeleportEvent event) {
         TeleportRequest teleportRequest = event.getTeleportRequest();
@@ -18,6 +19,15 @@ public class UserTeleportCombatListener implements Listener {
         Integer userId = teleportRequest.getUserId();
 
         CaribeUser caribeUser = FactionsCaribe.getInstance().getCaribeUserFactory().getUser(userId);
+
+        CaribeUser.Back back = new CaribeUser.Back(
+                caribeUser.getServerId(),
+                LocationSerialize.toString(
+                        caribeUser.getLocation()
+                )
+        );
+
+        caribeUser.setBack(back);
 
         if (caribeUser.inCombat())
             event.setCancelled(true);
