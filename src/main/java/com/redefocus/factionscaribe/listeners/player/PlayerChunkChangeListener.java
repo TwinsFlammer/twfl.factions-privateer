39bc7bac03b85dd7630af1cd00169468ebd5cb21
Gutyerrez <srgutyerrez@gmail.com>
2020-05-07 16:47:55 -0300

    Create PlayerChunkChangeListener.java

---
+++ src/main/java/com/redefocus/factionscaribe/listeners/player/PlayerChunkChangeListener.java
@@ -0,0 +1,32 @@
+package com.redefocus.factionscaribe.listeners.player;
+
+import com.redefocus.factionscaribe.FactionsCaribe;
+import com.redefocus.factionscaribe.user.data.CaribeUser;
+import org.bukkit.Chunk;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+/**
+ * @author SrGutyerrez
+ */
+public class PlayerChunkChangeListener implements Listener {
+    @EventHandler
+    public void onChange(PlayerMoveEvent event) {
+        Location fromLocation = event.getFrom();
+        Location toLocation = event.getTo();
+
+        Chunk fromChunk = fromLocation.getChunk();
+        Chunk toChunk = toLocation.getChunk();
+
+        if (!fromChunk.equals(toChunk)) {
+            Player player = event.getPlayer();
+
+            CaribeUser caribeUser = FactionsCaribe.getInstance().getCaribeUserFactory().getUser(player.getUniqueId());
+
+            caribeUser.updateScoreboardTitle();
+        }
+    }
+}
