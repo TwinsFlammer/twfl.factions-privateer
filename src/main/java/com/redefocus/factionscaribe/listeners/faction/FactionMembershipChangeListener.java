a6cf9176eb318c74e0d83de7f2ce450c1b391dcb
Gutyerrez <srgutyerrez@gmail.com>
2020-05-07 16:09:53 -0300

    Create FactionMembershipChangeListener.java

---
+++ src/main/java/com/redefocus/factionscaribe/listeners/faction/FactionMembershipChangeListener.java
@@ -0,0 +1,42 @@
+package com.redefocus.factionscaribe.listeners.faction;
+
+import com.massivecraft.factions.entity.MPlayer;
+import com.massivecraft.factions.event.EventFactionsMembershipChange;
+import com.redefocus.factionscaribe.FactionsCaribe;
+import com.redefocus.factionscaribe.user.data.CaribeUser;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+/**
+ * @author SrGutyerrez
+ */
+public class FactionMembershipChangeListener implements Listener {
+    @EventHandler
+    public void onJoin(EventFactionsMembershipChange event) {
+        MPlayer mPlayer = event.getMPlayer();
+
+        EventFactionsMembershipChange.MembershipChangeReason membershipChangeReason = event.getReason();
+
+        CaribeUser caribeUser = FactionsCaribe.getInstance().getCaribeUserFactory().getUser(mPlayer.getUuid());
+
+        switch (membershipChangeReason) {
+            case CREATE:
+            case JOIN: {
+                caribeUser.updateScoreboard(4, "");
+                caribeUser.updateScoreboard(5,  "");
+                caribeUser.updateScoreboard(6, "");
+                caribeUser.updateScoreboard(7, "");
+                caribeUser.updateScoreboard(8, "");
+            }
+            case LEAVE:
+            case KICK:
+            case DISBAND: {
+                caribeUser.removeScoreboard(4);
+                caribeUser.removeScoreboard(5);
+                caribeUser.removeScoreboard(6);
+                caribeUser.removeScoreboard(7);
+                caribeUser.removeScoreboard(8);
+            }
+        }
+    }
+}
