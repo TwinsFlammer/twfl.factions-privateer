c44e1d91193c23ae5644fca9a14a626718f03c97
Gutyerrez <srgutyerrez@gmail.com>
2020-05-08 16:57:05 -0300

    Update AsyncPlayerChatListener.java

--- src/main/java/com/redefocus/factionscaribe/chat/listener/AsyncPlayerChatListener.java
+++ src/main/java/com/redefocus/factionscaribe/chat/listener/AsyncPlayerChatListener.java
@@ -13,6 +13,7 @@ import com.redefocus.factionscaribe.user.data.CaribeUser;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
