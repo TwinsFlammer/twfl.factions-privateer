d7df32cd2260cd2f092b812557b112df573d366c
Gutyerrez <srgutyerrez@gmail.com>
2020-05-08 22:12:12 -0300

    Update HomeComponent.java

--- src/main/java/com/redefocus/factionscaribe/home/component/HomeComponent.java
+++ src/main/java/com/redefocus/factionscaribe/home/component/HomeComponent.java
@@ -4,9 +4,11 @@ import com.massivecraft.factions.entity.Faction;
 import com.redefocus.api.spigot.util.jsontext.data.JSONText;
 import com.redefocus.factionscaribe.home.data.Home;
 import com.redefocus.factionscaribe.user.data.CaribeUser;
+import org.bukkit.entity.Player;
 
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 /**
  * @author oNospher
@@ -14,44 +16,45 @@ import java.util.concurrent.atomic.AtomicInteger;
 public abstract class HomeComponent {
     private final JSONText jsonText;
 
-    public HomeComponent(List<Home> homes, Home.State state) {
+    public HomeComponent(List<Home> homeList, Home.State state) {
+        List<Home> homes = homeList.stream()
+                .filter(home -> home.getState().equals(state))
+                .collect(Collectors.toList());
+
         this.jsonText = new JSONText()
                 .text("§6Homes " + state.getName() + ": §f" + (homes.isEmpty() ? "Nenhuma." : ""))
                 .next();
 
         AtomicInteger index = new AtomicInteger(0);
 
-        if (homes.isEmpty())
-            this.jsonText.text("§fNenhuma.")
+        homes.forEach(home -> {
+            Faction faction = Faction.get(home.getFactionId());
+
+            Integer newIndex = index.getAndIncrement();
+
+            this.jsonText
+                    .text(home.getName())
+                    .hoverText(
+                            "§6Nome: §f" + home.getName() +
+                                    "\n" +
+                                    "§6Tipo: §f" + state.getName() +
+                                    "\n" +
+                                    "§6Facção: §f" + faction.getName() +
+                                    "\n" +
+                                    "§6Localização: §f" + home.getFancyLocation() +
+                                    "\n" +
+                                    "§6Mundo: §f" + home.getWorldName()
+                    )
+                    .execute("/home " + home.getName())
+                    .next()
+                    .text(newIndex + 1 == homes.size() ? "." : ", ")
                     .next();
-
-        homes.stream()
-                .filter(home -> home.getState().equals(state))
-                .forEach(home -> {
-                    Faction faction = Faction.get(home.getFactionId());
-
-                    Integer newIndex = index.getAndIncrement();
-
-                    this.jsonText
-                            .text(home.getName())
-                            .hoverText(
-                                    "§6Nome: §f" + home.getName() +
-                                            "\n" +
-                                            "§6Tipo: §f" + state.getName() +
-                                            "\n" +
-                                            "§6Facção: §f" + faction.getName() +
-                                            "\n" +
-                                            "§6Localização: §f" + home.getFancyLocation() +
-                                            "\n" +
-                                            "§6Mundo: §f" + home.getWorldName()
-                            )
-                            .next()
-                            .text(newIndex + 1 == homes.size() ? "." : ", ")
-                            .next();
-                });
+        });
     }
 
     public void send(CaribeUser caribeUser) {
-        caribeUser.sendMessage(this.jsonText);
+        Player player = caribeUser.getPlayer();
+
+        this.jsonText.send(player);
     }
 }
