7cc9a2cd53ed4a64b7078c11005f744e32785759
Gutyerrez <srgutyerrez@gmail.com>
2020-05-09 20:55:58 -0300

    Create TpaRequest.java

---
+++ src/main/java/com/redefocus/factionscaribe/commands/player/tpa/data/TpaRequest.java
@@ -0,0 +1,58 @@
+package com.redefocus.factionscaribe.commands.player.tpa.data;
+
+import com.redefocus.api.spigot.SpigotAPI;
+import com.redefocus.factionscaribe.commands.player.tpa.channel.TpaChannel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.json.simple.JSONObject;
+
+import java.util.UUID;
+
+/**
+ * @author SrGutyerrez
+ */
+@AllArgsConstructor
+public class TpaRequest {
+    @Getter
+    private final UUID uuid = UUID.randomUUID();
+    @Getter
+    private final Integer userId, targetId, rootServerId;
+
+    @Getter
+    @Setter
+    private Action action;
+
+    @Getter
+    private final Long expireTime;
+
+    public void publish() {
+        TpaChannel tpaChannel = new TpaChannel();
+
+        tpaChannel.sendMessage(this.toString());
+    }
+
+    public Boolean hasExpired() {
+        return this.expireTime <= System.currentTimeMillis();
+    }
+
+    @Override
+    public String toString() {
+        JSONObject jsonObject = new JSONObject();
+
+        jsonObject.put("user_id", this.userId);
+        jsonObject.put("target_id", this.targetId);
+        jsonObject.put("root_server_id", SpigotAPI.getRootServerId());
+        jsonObject.put("action", this.action);
+        jsonObject.put("expire_time", System.currentTimeMillis());
+
+        return jsonObject.toString();
+    }
+
+    public static enum Action {
+        SEND,
+        CANCEL,
+        ACCEPT,
+        DENY;
+    }
+}
