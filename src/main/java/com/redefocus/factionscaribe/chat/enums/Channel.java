baac007c022b9f8a499dab1dab0c3017ac422569
Gutyerrez <srgutyerrez@gmail.com>
2020-05-09 03:39:31 -0300

    Update Channel.java

--- src/main/java/com/redefocus/factionscaribe/chat/enums/Channel.java
+++ src/main/java/com/redefocus/factionscaribe/chat/enums/Channel.java
@@ -5,40 +5,75 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.bukkit.ChatColor;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
  * @author SrGutyerrez
  */
 @RequiredArgsConstructor
-@Getter
 public enum Channel {
     LOCAL(
+            "local",
+            new String[]{
+                    "l"
+            },
             "l",
             ChatColor.YELLOW,
             TimeUnit.SECONDS.toMillis(3)
     ),
     GLOBAL(
+            "global",
+            new String[]{
+                    "g"
+            },
             "g",
             ChatColor.GRAY,
             TimeUnit.SECONDS.toMillis(10)
     ),
     FACTION(
             "",
+            new String[]{},
+            "",
             ChatColor.AQUA,
             TimeUnit.SECONDS.toMillis(3)
     ),
     ALLY(
+            "",
+            new String[]{},
             "§e[§a%s§e]",
             ChatColor.WHITE,
             TimeUnit.SECONDS.toMillis(3)
     );
 
+    @Getter
+    private final String name;
+
+    private final String[] aliases;
+
+    @Getter
     private final String prefix;
+    @Getter
     private final ChatColor color;
+    @Getter
     private final Long delay;
 
     public String getHKey() {
         return "chat_" + SpigotAPI.getRootServerId() + "_" + this.name();
+    }
+
+    public List<String> getAliases() {
+        return Arrays.asList(this.aliases);
+    }
+
+    public static Channel getChannel(String name) {
+        for (Channel channel : Channel.values()) {
+            if (channel.toString().equalsIgnoreCase(name)
+                    || channel.getName().equalsIgnoreCase(name)
+                    || channel.getAliases().contains(name.toLowerCase())) return channel;
+        }
+
+        return null;
     }
 }
