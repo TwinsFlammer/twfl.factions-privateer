4fd41dec2274f9531f08752a7404500f71c95eb1
Gutyerrez <srgutyerrez@gmail.com>
2020-05-10 16:12:52 -0300

    Create SpecialItemListener.java

---
+++ src/main/java/com/redefocus/factionscaribe/specialitem/listener/SpecialItemListener.java
@@ -0,0 +1,34 @@
+package com.redefocus.factionscaribe.specialitem.listener;
+
+import com.redefocus.factionscaribe.specialitem.data.AbstractSpecialItem;
+import com.redefocus.factionscaribe.specialitem.manager.SpecialItemManager;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.function.Consumer;
+
+/**
+ * @author SrGutyerrez
+ */
+public class SpecialItemListener implements Listener {
+    @EventHandler
+    public void onInteract(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+
+        ItemStack itemStack = player.getItemInHand();
+
+        AbstractSpecialItem abstractSpecialItem = SpecialItemManager.getSpecialItem(itemStack);
+
+        if (abstractSpecialItem == null) return;
+
+        if (abstractSpecialItem.getEventConsumer() != null) {
+            Consumer<PlayerInteractEvent> eventConsumer = abstractSpecialItem.getEventConsumer();
+
+            if (eventConsumer instanceof PlayerInteractEvent)
+                eventConsumer.accept(event);
+        }
+    }
+}
