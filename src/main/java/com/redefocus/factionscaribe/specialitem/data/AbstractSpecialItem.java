3afe630adcc0c75859f9845db5b23edf90b55af5
Gutyerrez <srgutyerrez@gmail.com>
2020-05-10 16:11:12 -0300

    Create AbstractSpecialItem.java

---
+++ src/main/java/com/redefocus/factionscaribe/specialitem/data/AbstractSpecialItem.java
@@ -0,0 +1,20 @@
+package com.redefocus.factionscaribe.specialitem.data;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.bukkit.event.Event;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.function.Consumer;
+
+/**
+ * @author SrGutyerrez
+ */
+@RequiredArgsConstructor
+public abstract class AbstractSpecialItem<E extends Event> {
+    @Getter
+    private final String name;
+
+    public abstract ItemStack getItemStack();
+    public abstract Consumer<E> getEventConsumer();
+}
