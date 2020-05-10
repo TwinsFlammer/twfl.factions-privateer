53e140bb404111e0859deb1b754f8bb020a70c2a
Gutyerrez <srgutyerrez@gmail.com>
2020-05-10 16:12:41 -0300

    Create LauncherSpecialITem.java

---
+++ src/main/java/com/redefocus/factionscaribe/specialitem/defaults/LauncherSpecialITem.java
@@ -0,0 +1,42 @@
+package com.redefocus.factionscaribe.specialitem.defaults;
+
+import com.redefocus.api.spigot.inventory.item.CustomItem;
+import com.redefocus.factionscaribe.specialitem.data.AbstractSpecialItem;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.function.Consumer;
+
+/**
+ * @author SrGutyerrez
+ */
+public class LauncherSpecialITem<E extends PlayerInteractEvent> extends AbstractSpecialItem<E> {
+    public LauncherSpecialITem() {
+        super(
+                "lançador"
+        );
+    }
+
+    @Override
+    public ItemStack getItemStack() {
+        return new CustomItem(Material.FIREWORK)
+                .name("§bLançador")
+                .lore("§7Começando o role")
+                .build();
+    }
+
+    @Override
+    public Consumer<E> getEventConsumer() {
+        return (event) -> {
+            Player player = event.getPlayer();
+
+            ItemStack itemStack = player.getItemInHand();
+
+            if (itemStack != null && itemStack.isSimilar(this.getItemStack())) {
+                player.sendMessage("Testando...");
+            }
+        };
+    }
+}
