0ef30767b572e6d5ed20ef6eb2d5d0f04b8b6e57
Gutyerrez <srgutyerrez@gmail.com>
2020-05-11 20:52:34 -0300

    Update SpecialItemListener.java

--- src/main/java/com/redefocus/factionscaribe/specialitem/listener/SpecialItemListener.java
+++ src/main/java/com/redefocus/factionscaribe/specialitem/listener/SpecialItemListener.java
@@ -1,14 +1,20 @@
 package com.redefocus.factionscaribe.specialitem.listener;
 
+import com.redefocus.factionscaribe.FactionsCaribe;
 import com.redefocus.factionscaribe.specialitem.data.AbstractSpecialItem;
+import com.redefocus.factionscaribe.specialitem.defaults.LauncherSpecialItem;
 import com.redefocus.factionscaribe.specialitem.manager.SpecialItemManager;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.function.Consumer;
+import java.util.function.BiConsumer;
 
 /**
  * @author SrGutyerrez
@@ -27,9 +33,28 @@ public class SpecialItemListener implements Listener {
         if (!abstractSpecialItem.getEventType().equals(PlayerInteractEvent.class)) return;
 
         if (abstractSpecialItem.getEventConsumer() != null) {
-            Consumer<PlayerInteractEvent> eventConsumer = abstractSpecialItem.getEventConsumer();
+            BiConsumer<PlayerInteractEvent, Event> eventConsumer = abstractSpecialItem.getEventConsumer();
 
-            eventConsumer.accept(event);
+            eventConsumer.accept(event, null);
+        }
+    }
+
+    @EventHandler
+    public void onDamage(EntityDamageEvent event) {
+        Entity entity = event.getEntity();
+
+        if (entity.getType() != EntityType.PLAYER) return;
+
+        EntityDamageEvent.DamageCause damageCause = event.getCause();
+
+        if (damageCause == EntityDamageEvent.DamageCause.FALL) {
+            Player player = (Player) entity;
+
+            if (player.hasMetadata(LauncherSpecialItem.METADATA)) {
+                player.removeMetadata(LauncherSpecialItem.METADATA, FactionsCaribe.getInstance());
+
+                event.setCancelled(true);
+            }
         }
     }
 }
