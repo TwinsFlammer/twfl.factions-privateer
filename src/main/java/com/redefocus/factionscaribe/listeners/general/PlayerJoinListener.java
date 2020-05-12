944f723078447b8c3917daecd7ded8854afb1699
Gutyerrez <srgutyerrez@gmail.com>
2020-05-12 19:07:45 -0300

    Update PlayerJoinListener.java

--- src/main/java/com/redefocus/factionscaribe/listeners/general/PlayerJoinListener.java
+++ src/main/java/com/redefocus/factionscaribe/listeners/general/PlayerJoinListener.java
@@ -10,7 +10,7 @@ import org.bukkit.event.player.PlayerJoinEvent;
 /**
  * @author SrGutyerrez
  */
-public class PlayerJoinListener {
+public class PlayerJoinListener implements Listener {
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
