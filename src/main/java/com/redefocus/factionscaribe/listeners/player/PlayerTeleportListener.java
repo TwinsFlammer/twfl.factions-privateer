9f8508a15483596e33aeee96ee8f372acc21624b
Gutyerrez <srgutyerrez@gmail.com>
2020-05-16 04:31:45 -0300

    Update PlayerTeleportListener.java

--- src/main/java/com/redefocus/factionscaribe/listeners/player/PlayerTeleportListener.java
+++ src/main/java/com/redefocus/factionscaribe/listeners/player/PlayerTeleportListener.java
@@ -1,5 +1,7 @@
 package com.redefocus.factionscaribe.listeners.player;
 
+import com.redefocus.factionscaribe.FactionsCaribe;
+import com.redefocus.factionscaribe.user.data.CaribeUser;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.WorldBorder;
