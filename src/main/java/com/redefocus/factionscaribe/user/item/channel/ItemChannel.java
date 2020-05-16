10fe6c609670ca567d8a778b4748d3f86b577ca3
Gutyerrez <srgutyerrez@gmail.com>
2020-05-16 04:29:28 -0300

    Create ItemChannel.java

---
+++ src/main/java/com/redefocus/factionscaribe/user/item/channel/ItemChannel.java
@@ -0,0 +1,29 @@
+package com.redefocus.factionscaribe.user.item.channel;
+
+import com.redefocus.common.shared.databases.redis.channel.data.Channel;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.exceptions.JedisDataException;
+
+/**
+ * @author SrGutyerrez
+ */
+public class ItemChannel extends Channel {
+    public static final String CHANNEL_NAME = "item_channel";
+
+    @Override
+    public String getName() {
+        return ItemChannel.CHANNEL_NAME;
+    }
+
+    @Override
+    public void sendMessage(String message) {
+        try (Jedis jedis = this.getJedisPool().getResource()) {
+            jedis.publish(
+                    this.getName(),
+                    message
+            );
+        } catch (JedisDataException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
