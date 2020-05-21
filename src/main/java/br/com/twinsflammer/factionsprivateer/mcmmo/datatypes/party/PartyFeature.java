4d66e172014011d34c2f5a6a0f544ab1afb724d2
Gutyerrez <srgutyerrez@gmail.com>
2020-05-21 13:14:45 -0300

    Update PartyFeature.java

--- src/main/java/br/com/twinsflammer/factionsprivateer/mcmmo/datatypes/party/PartyFeature.java
+++ src/main/java/br/com/twinsflammer/factionsprivateer/mcmmo/datatypes/party/PartyFeature.java
@@ -1,6 +1,5 @@
 package br.com.twinsflammer.factionsprivateer.mcmmo.datatypes.party;
 
-import br.com.twinsflammer.factionsprivateer.mcmmo.commands.party.PartySubcommandType;
 import br.com.twinsflammer.factionsprivateer.mcmmo.config.Config;
 import br.com.twinsflammer.factionsprivateer.mcmmo.locale.LocaleLoader;
 import br.com.twinsflammer.factionsprivateer.mcmmo.util.Permissions;
@@ -24,27 +23,6 @@ public enum PartyFeature {
     }
 
     public boolean hasPermission(Player player) {
-        PartySubcommandType partySubCommandType;
-        switch (this) {
-            case CHAT:
-                partySubCommandType = PartySubcommandType.CHAT;
-                break;
-            case TELEPORT:
-                partySubCommandType = PartySubcommandType.TELEPORT;
-                break;
-            case ALLIANCE:
-                partySubCommandType = PartySubcommandType.ALLIANCE;
-                break;
-            case ITEM_SHARE:
-                partySubCommandType = PartySubcommandType.ITEMSHARE;
-                break;
-            case XP_SHARE:
-                partySubCommandType = PartySubcommandType.XPSHARE;
-                break;
-            default:
-                return false;
-        }
-
-        return Permissions.partySubcommand(player, partySubCommandType);
+        return false;
     }
 }
