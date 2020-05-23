4b2bfacb6479befba6beef65eae20bd0ca118bf1
Gutyerrez <srgutyerrez@gmail.com>
2020-05-23 09:22:09 -0300

    Update SpawnerCommand.java

--- src/main/java/br/com/twinsflammer/factionsprivateer/spawner/command/SpawnerCommand.java
+++ src/main/java/br/com/twinsflammer/factionsprivateer/spawner/command/SpawnerCommand.java
@@ -2,14 +2,23 @@ package br.com.twinsflammer.factionsprivateer.spawner.command;
 
 import br.com.twinsflammer.api.spigot.commands.CustomCommand;
 import br.com.twinsflammer.api.spigot.commands.enums.CommandRestriction;
+import br.com.twinsflammer.api.spigot.util.NBTTag;
 import br.com.twinsflammer.common.shared.permissions.group.GroupNames;
 import br.com.twinsflammer.common.shared.permissions.user.data.User;
 import br.com.twinsflammer.common.shared.permissions.user.manager.UserManager;
 import br.com.twinsflammer.common.shared.util.Helper;
 import br.com.twinsflammer.factionsprivateer.FactionsPrivateer;
+import br.com.twinsflammer.factionsprivateer.spawner.item.Spawner;
+import br.com.twinsflammer.factionsprivateer.spawner.manager.SpawnerManager;
+import br.com.twinsflammer.factionsprivateer.spawner.stacker.listener.SpawnerSpawnListener;
 import br.com.twinsflammer.factionsprivateer.user.data.PrivateerUser;
+import net.minecraft.server.v1_8_R3.NBTBase;
+import net.minecraft.server.v1_8_R3.NBTTagCompound;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.EntityType;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Arrays;
 
 /**
  * @author SrGutyerrez
@@ -26,7 +35,7 @@ public class SpawnerCommand extends CustomCommand {
     @Override
     public void onCommand(CommandSender commandSender, User user, String[] args) {
         if (args.length != 4 || !args[0].equalsIgnoreCase("give")) {
-            commandSender.sendMessage("§cUtilize /spawner give <usuário> <item> <quantia>.");
+            commandSender.sendMessage("§cUtilize /spawner give <usuário> <tipo> <quantia>.");
             return;
         }
 
@@ -52,7 +61,12 @@ public class SpawnerCommand extends CustomCommand {
         }
 
         try {
-            EntityType entityType = EntityType.valueOf(args[2]);
+            EntityType entityType = EntityType.valueOf(args[2].toUpperCase());
+
+            if (!Arrays.asList(SpawnerSpawnListener.ALLOWED_ENTITY_TYPES).contains(entityType)) {
+                commandSender.sendMessage("§cEste tipo de gerador não é permitido.");
+                return;
+            }
 
             Integer amount = Helper.isInteger(args[3]) ? Integer.parseInt(args[3]) : null;
 
@@ -61,9 +75,11 @@ public class SpawnerCommand extends CustomCommand {
                 return;
             }
 
+            ItemStack spawner = SpawnerManager.createSpawner(entityType, amount);
 
-        } catch (NullPointerException exception) {
-            commandSender.sendMessage("§c");
+            privateerUser.giveItem(spawner);
+        } catch (IllegalArgumentException exception) {
+            commandSender.sendMessage("§cEste gerador não existe.");
         }
     }
 }
