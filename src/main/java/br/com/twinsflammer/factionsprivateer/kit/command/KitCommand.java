9fe921a86e936f91a904a9d369345c56d94f4d63
Gutyerrez <srgutyerrez@gmail.com>
2020-05-23 13:42:26 -0300

    Update KitCommand.java

--- src/main/java/br/com/twinsflammer/factionsprivateer/kit/command/KitCommand.java
+++ src/main/java/br/com/twinsflammer/factionsprivateer/kit/command/KitCommand.java
@@ -1,7 +1,31 @@
 package br.com.twinsflammer.factionsprivateer.kit.command;
 
+import br.com.twinsflammer.api.spigot.commands.CustomCommand;
+import br.com.twinsflammer.api.spigot.commands.enums.CommandRestriction;
+import br.com.twinsflammer.common.shared.permissions.group.GroupNames;
+import br.com.twinsflammer.common.shared.permissions.user.data.User;
+import br.com.twinsflammer.factionsprivateer.kit.inventory.KitInventory;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
 /**
  * @author SrGutyerrez
  */
-public class KitCommand {
+public class KitCommand extends CustomCommand {
+    public KitCommand() {
+        super(
+                "kit",
+                CommandRestriction.IN_GAME,
+                GroupNames.DEFAULT
+        );
+    }
+
+    @Override
+    public void onCommand(CommandSender commandSender, User user, String[] strings) {
+        Player player = (Player) commandSender;
+
+        player.openInventory(
+                new KitInventory()
+        );
+    }
 }
