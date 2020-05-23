f624aafeaf17c9a2a04a6e381407eddbd3b46a8f
Gutyerrez <srgutyerrez@gmail.com>
2020-05-23 04:32:52 -0300

    Update PlayerDeathCombatListener.java

--- src/main/java/br/com/twinsflammer/factionsprivateer/combat/listener/PlayerDeathCombatListener.java
+++ src/main/java/br/com/twinsflammer/factionsprivateer/combat/listener/PlayerDeathCombatListener.java
@@ -1,5 +1,7 @@
 package br.com.twinsflammer.factionsprivateer.combat.listener;
 
+import br.com.twinsflammer.api.spigot.SpigotAPI;
+import br.com.twinsflammer.api.spigot.util.serialize.LocationSerialize;
 import br.com.twinsflammer.factionsprivateer.FactionsPrivateer;
 import br.com.twinsflammer.factionsprivateer.user.data.PrivateerUser;
 import org.bukkit.entity.Player;
@@ -11,7 +13,6 @@ import org.bukkit.event.entity.PlayerDeathEvent;
  * @author oNospher
  **/
 public class PlayerDeathCombatListener implements Listener {
-
     @EventHandler
     public void onDeath(PlayerDeathEvent event) {
         Player player = event.getEntity();
@@ -22,5 +23,12 @@ public class PlayerDeathCombatListener implements Listener {
 
         privateerUser.setCombatDuration(0L);
         privateerUser1.setCombatDuration(0L);
+
+        PrivateerUser.Back back = new PrivateerUser.Back(
+                SpigotAPI.getCurrentServer().getId(),
+                LocationSerialize.toString(player.getLocation())
+        );
+
+        privateerUser.setBack(back);
     }
 }
