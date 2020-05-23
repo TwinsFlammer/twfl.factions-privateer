97eb166d800f34e72856485dafe54943e27ba4ac
Gutyerrez <srgutyerrez@gmail.com>
2020-05-23 07:31:48 -0300

    Update EntityDeathListener.java

--- src/main/java/br/com/twinsflammer/factionsprivateer/spawner/stacker/listener/EntityDeathListener.java
+++ src/main/java/br/com/twinsflammer/factionsprivateer/spawner/stacker/listener/EntityDeathListener.java
@@ -1,10 +1,18 @@
 package br.com.twinsflammer.factionsprivateer.spawner.stacker.listener;
 
+import br.com.twinsflammer.factionsprivateer.util.BlockUtil;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.CreatureSpawner;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.Arrays;
 
@@ -13,18 +21,48 @@ import java.util.Arrays;
  */
 public class EntityDeathListener implements Listener {
     private final Material[] BLACKLISTED_TO_DROP = {
-            Material.RED_ROSE,
-            Material.ROTTEN_FLESH
+            Material.RED_ROSE
     };
 
     @EventHandler
     public void onDeath(EntityDeathEvent event) {
-        Entity entity = event.getEntity();
+        LivingEntity livingEntity = event.getEntity();
 
-        if (!Arrays.asList(SpawnerSpawnListener.ALLOWED_ENTITY_TYPES).contains(entity.getType()))
+        Entity killer = livingEntity.getKiller();
+
+        if (!Arrays.asList(SpawnerSpawnListener.ALLOWED_ENTITY_TYPES).contains(livingEntity.getType()))
             return;
 
-        Integer amount = entity.hasMetadata(SpawnerSpawnListener.STACK_METADATA) ? entity.getMetadata(SpawnerSpawnListener.STACK_METADATA).get(0).asInt() : 1;
+        Integer amount = livingEntity.hasMetadata(SpawnerSpawnListener.STACK_METADATA) ? livingEntity.getMetadata(SpawnerSpawnListener.STACK_METADATA).get(0).asInt() : 1;
+
+        Integer multiplier = this.getSpawnerMultiplier(
+                livingEntity.getType(),
+                livingEntity.getLocation()
+        );
+
+        if (multiplier > amount) multiplier = amount;
+
+        if (amount > multiplier)
+            SpawnerSpawnListener.updateAmount(
+                    livingEntity,
+                    amount - multiplier
+            );
+
+        if (killer != null && killer.getType() == EntityType.PLAYER) {
+            Player player = (Player) killer;
+
+            ItemStack itemStack = player.getItemInHand();
+
+            if (itemStack.getType().name().endsWith("_SWORD")) {
+                Integer lootingEnchantmentLevel = itemStack.containsEnchantment(Enchantment.LOOT_BONUS_MOBS) ? itemStack.getEnchantmentLevel(Enchantment.LOOT_BONUS_MOBS) : 0;
+
+                if (lootingEnchantmentLevel != 0) {
+                    multiplier = multiplier * (int) (lootingEnchantmentLevel > 1 ? lootingEnchantmentLevel / 1.5D : 1);
+                }
+            }
+        }
+
+        Integer dropMultiplier = multiplier == 0 ? 1 : multiplier;
 
         event.getDrops().forEach(itemStack -> {
             Material material = itemStack.getType();
@@ -33,9 +71,22 @@ public class EntityDeathListener implements Listener {
                 itemStack.setType(Material.AIR);
             else {
                 itemStack.setAmount(
-                        itemStack.getAmount() * amount
+                        itemStack.getAmount() * dropMultiplier
                 );
             }
         });
+    }
+
+    private Integer getSpawnerMultiplier(EntityType entityType, Location location) {
+        return (int) BlockUtil.getNearbyBlocks(location, 32)
+                .stream()
+                .filter(block -> block.getType() == Material.MOB_SPAWNER)
+                .filter(block -> block.getY() >= location.getY())
+                .filter(block -> {
+                    CreatureSpawner creatureSpawner = (CreatureSpawner) block.getState();
+
+                    return creatureSpawner.getSpawnedType().equals(entityType);
+                })
+                .count();
     }
 }
