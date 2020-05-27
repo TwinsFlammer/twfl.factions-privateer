139b6734dc84e8a4b26a0e1be062a95682104613
Gutyerrez <srgutyerrez@gmail.com>
2020-05-27 04:54:26 -0300

    Update FactionsPrivateer.java

--- src/main/java/br/com/twinsflammer/factionsprivateer/FactionsPrivateer.java
+++ src/main/java/br/com/twinsflammer/factionsprivateer/FactionsPrivateer.java
@@ -1,7 +1,9 @@
 package br.com.twinsflammer.factionsprivateer;
 
 import br.com.twinsflammer.api.shared.API;
+import br.com.twinsflammer.api.spigot.SpigotAPI;
 import br.com.twinsflammer.api.spigot.TwinsPlugin;
+import br.com.twinsflammer.api.spigot.restart.data.Restart;
 import br.com.twinsflammer.common.shared.permissions.user.manager.UserManager;
 import br.com.twinsflammer.factionsprivateer.cash.event.CashChangeEvent;
 import br.com.twinsflammer.factionsprivateer.manager.StartManager;
@@ -9,8 +11,10 @@ import br.com.twinsflammer.factionsprivateer.mcmmo.mcMMO;
 import br.com.twinsflammer.factionsprivateer.user.data.PrivateerUser;
 import br.com.twinsflammer.factionsprivateer.user.factory.PrivateerUserFactory;
 import lombok.Getter;
+import org.bukkit.Bukkit;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author SrGutyerrez
@@ -44,6 +48,22 @@ public class FactionsPrivateer extends TwinsPlugin {
         CashChangeEvent cashChangeEvent = new CashChangeEvent();
 
         API.getInstance().setICashChangeEvent(cashChangeEvent);
+
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(
+                this,
+                () -> {
+                    if (FactionsPrivateer.startTime >= (FactionsPrivateer.startTime + TimeUnit.HOURS.toMillis(2)) && SpigotAPI.getInstance().getRestart() == null) {
+                        Restart restart = new Restart(
+                                System.currentTimeMillis(),
+                                System.currentTimeMillis() + TimeUnit.MINUTES.toMillis(3)
+                        );
+
+                        restart.start();
+                    }
+                },
+                0,
+                20
+        );
     }
 
     @Override
